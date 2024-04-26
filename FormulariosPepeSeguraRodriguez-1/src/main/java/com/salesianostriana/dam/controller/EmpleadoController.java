dc7e735dfa163f6587e56e9dffb0577f35395f4f
José Rafael <pepesegurarodriguez@gmail.com>
2024-04-26 11:00:04 +0200

    thymeleaf formularios

--- FormulariosPepeSeguraRodriguez-1/src/main/java/com/salesianostriana/dam/controller/EmpleadoController.java
+++ FormulariosPepeSeguraRodriguez-1/src/main/java/com/salesianostriana/dam/controller/EmpleadoController.java
@@ -1,8 +1,29 @@
 package com.salesianostriana.dam.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.salesianostriana.dam.model.Empleado;
+
+
 
 @Controller
 public class EmpleadoController {
-
+	
+	//Método get
+	@GetMapping("/empleado")
+	public String mostrarFormulario(Model model) {
+		Empleado e = new Empleado();
+		model.addAttribute("empleadoForm", e);
+		return "login";
+	}
+	//Metodo post
+	@PostMapping("/addEmpleado")
+	public String submit(@ModelAttribute("empleadoForm") Empleado e, Model model) {
+		model.addAttribute("empleado",e);
+		return "view"; //recordar que en el return view significa una pagina de html 
+	}
 }
